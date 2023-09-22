ec08f6e9f32d2a6213e804f908fb5e5e65ef3367
Adrian Yates <adeyatz@gmail.com>
2023-09-22 18:53:09 +0100

    Seat: Unit test for allocate() method written, all tests fail

--- src/main/java/org/cinnamon/cinema/Seat.java
+++ src/main/java/org/cinnamon/cinema/Seat.java
@@ -3,4 +3,15 @@ package org.cinnamon.cinema;
 public class Seat {
     public Seat(String a, int i) {
     }
+
+    public String getPosition() {
+        return null;
+    }
+
+    public boolean isAllocated() {
+        return false;
+    }
+
+    public void allocate() {
+    }
 }
--- src/test/java/org/cinnamon/cinema/SeatTest.java
+++ src/test/java/org/cinnamon/cinema/SeatTest.java
@@ -6,16 +6,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SeatTest {
 
-    @Test static void testSeatCreation () {
+    @Test void testSeatCreation () {
         Seat seat = new Seat("A", 1);
 
         assertEquals(seat.getPosition(), "A1");
     }
 
-    @Test static void testSeatIsNotAllocated () {
+    @Test void testSeatIsNotAllocated () {
         Seat seat = new Seat("A", 1);
 
         assertFalse(seat.isAllocated());
+    }
+
+
+    @ Test void testSeatIsAllocated () {
+        Seat seat = new Seat ("A", 1);
+
+        seat.allocate();
+
+        assertTrue (seat.isAllocated());
     }
 
 }

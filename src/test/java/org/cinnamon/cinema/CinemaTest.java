f3b1ed86ed3a28a49ca636ad73be320bcb6c5213
Adrian Yates <adeyatz@gmail.com>
2023-09-23 00:53:15 +0100

    CinemaTest(): Added testThreeByFiveUntilFull() to completely fill the seats with random numbers and print results to the console

--- src/test/java/org/cinnamon/cinema/CinemaTest.java
+++ src/test/java/org/cinnamon/cinema/CinemaTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -56,5 +58,91 @@ class CinemaTest {
         assertEquals("A1", seats.get(0).getPosition());
         assertEquals("B1", seats.get(1).getPosition());
     }
+
+    @Test
+    void testBookSeatsTooMany () {
+        testCinema = new Cinema(1, 10);
+
+        List<Seat> seats = testCinema.bookSeats (4);
+
+        assertNull(seats);
+    }
+
+    @Test
+    void testBookSeatsTooFew () {
+        testCinema = new Cinema(1, 10);
+
+        List<Seat> seats = testCinema.bookSeats (0);
+
+        assertNull(seats);
+    }
+
+
+
+    @Test
+    void testBookSeatsTooManyForSeatCount () {
+        testCinema = new Cinema(1, 2);
+
+        List<Seat> seats = testCinema.bookSeats (3);
+
+        assertNull(seats);
+    }
+
+
+    @Test
+    void testBookSeatsThreeRowsByFive () {
+        testCinema = new Cinema(3, 5);
+
+        List<Seat> seats = testCinema.bookSeats (2);
+
+        assertEquals(2, seats.size());
+        assertEquals("A1", seats.get(0).getPosition());
+        assertEquals("A2", seats.get(1).getPosition());
+
+        seats = testCinema.bookSeats (1);
+
+        assertEquals(1, seats.size());
+        assertEquals("A3", seats.get(0).getPosition());
+
+        seats = testCinema.bookSeats (3);
+
+        assertEquals(3, seats.size());
+        assertEquals("A4", seats.get(0).getPosition());
+        assertEquals("A5", seats.get(1).getPosition());
+        assertEquals("B5", seats.get(2).getPosition());
+
+        List<Seat> freeSeats = testCinema.requestFreeSeats();
+        assertEquals(9, freeSeats.size());
+        assertEquals("B4", freeSeats.get(0).getPosition());
+
+        List<Seat> occupiedSeats = testCinema.requestOccupiedSeats();
+        assertEquals(6, occupiedSeats.size());
+        assertEquals("A1", occupiedSeats.get(0).getPosition());
+        assertEquals("B5", occupiedSeats.get(occupiedSeats.size() - 1).getPosition());
+
+    }
+
+
+    Supplier <Integer> nextTicketsSize = () -> Math.toIntExact(Math.round(Math.random() * 2) + 1);
+
+    @Test
+    void testThreeByFiveUntilFull() {
+        testCinema = new Cinema(3, 5);
+        List<Seat> seats;
+
+        do {
+            int size = nextTicketsSize.get();
+            System.out.println("Size:" + size);
+
+            seats = testCinema.bookSeats (size);
+            if (seats == null) {
+                System.out.println("Failed to get seats");
+            } else {
+                seats.stream().forEach(seat -> System.out.println(seat.getPosition()));
+            }
+
+        } while (seats != null);
+    }
+
 
 }

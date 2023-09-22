bb347660df0313250612ec46ab7246358ee2d025
Adrian Yates <adeyatz@gmail.com>
2023-09-22 22:47:26 +0100

    Cinema(): Implement and test methods requestFreeSeats() and requestOccupiedSeats()

--- src/main/java/org/cinnamon/cinema/Cinema.java
+++ src/main/java/org/cinnamon/cinema/Cinema.java
@@ -4,15 +4,21 @@ import java.util.Collection;
 import java.util.List;
 
 public class Cinema {
-    public Cinema(int i, int i1) {
+
+    private List <Seat> seats;
+
+    public Cinema(int rowsCount, int seatPerRowCount) {
+        SeatPlanner planner = new SeatPlannerZigZag();
+
+        seats = planner.getSeats(rowsCount, seatPerRowCount);
     }
 
     public List<Seat> requestFreeSeats() {
-        return null;
+        return seats.stream().filter(seat -> !seat.isAllocated()).toList();
     }
 
     public List<Seat>  requestOccupiedSeats() {
-        return null;
+        return seats.stream().filter(seat -> seat.isAllocated()).toList();
     }
 
     public List<Seat> bookSeats(int i) {

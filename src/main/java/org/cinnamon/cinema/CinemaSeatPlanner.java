56f9eecb8d8d3eab9808169e3371b34d0403b277
Adrian Yates <adeyatz@gmail.com>
2023-09-22 19:17:37 +0100

    CinemaSeatPlanner(): First test passes, but incorrect implementation

--- src/main/java/org/cinnamon/cinema/CinemaSeatPlanner.java
+++ src/main/java/org/cinnamon/cinema/CinemaSeatPlanner.java
@@ -1,10 +1,19 @@
 package org.cinnamon.cinema;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CinemaSeatPlanner implements SeatPlanner {
     @Override
     public List<Seat> getSeats(int rowCount, int seatCount) {
-        return null;
+
+        if (rowCount == 0 || seatCount == 0)
+            throw new IllegalArgumentException("Cannot have 0 rows or seats");
+
+        List <Seat> seats = new ArrayList<Seat>();
+
+        seats.add(new Seat("A", 1));
+
+        return seats;
     }
 }
